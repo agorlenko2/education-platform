401662c96c2b6e8a15a9ab76b51dcbaeeafe1433
Alexander Gorlenko <agorlenko4@gmail.com>
2023-10-26 20:25:58 +0300

    Added content management system

--- education_platform/courses/views.py
+++ education_platform/courses/views.py
@@ -7,8 +7,10 @@ from django.contrib.auth.mixins import (
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
+from django.apps import apps
+from django.forms import modelform_factory
 
-from .models import Course
+from .models import Course, Module, Content
 from .forms import ModuleFormSet
 
 
@@ -108,5 +110,111 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             {
                 'course': self.course,
                 'formset': formset
+            }
+        )
+
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+
+    def get_model(self, model_name):
+        """
+        Get model class for a model name.
+        """
+        if model_name in ('text', 'video', 'image', 'file',):
+            return apps.get_model(app_label='courses', model_name=model_name)
+
+    def get_form(self, model, *args, **kwargs):
+        """
+        Create dynamic form for a model excluding specific fields.
+        """
+        form = modelform_factory(
+            model,
+            exclude=(
+                'owner', 'order', 'created', 'update',
+            )
+        )
+        return form(*args, **kwargs)
+
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(
+            Module,
+            id=module_id,
+            course__owner=request.user
+        )
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(
+                self.model,
+                id=id,
+                owner=request.user
+            )
+        return super().dispatch(request, module_id, model_name, id)
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response(
+            {
+                'form': form,
+                'object': self.obj
+            }
+        )
+
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(
+            self.model,
+            instance=self.obj,
+            data=request.POST,
+            files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                Content.objects.create(module=self.module, item=obj)
+            return redirect(
+                reverse('courses:module_content_list', args=(module_id, ))
+            )
+        return self.render_to_response(
+            {
+                'form': form,
+                'object': self.obj
+            }
+        )
+
+
+class ContentDeleteView(View):
+    def post(self, request, id):
+        content = get_object_or_404(
+            Content,
+            id=id,
+            module__course__owner=request.user
+        )
+        module = content.module
+        content.item.delete()
+        content.delete()
+        return redirect(
+            reverse(
+                'courses:module_content_list',
+                args=(module.id, )
+            )
+        )
+
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+
+    def get(self, request, module_id):
+        module = get_object_or_404(
+            Module,
+            id=module_id,
+            course__owner=request.user
+        )
+        return self.render_to_response(
+            {
+                'module': module
             }
         )
